600072046270db67952040ea003310343d6433c9
DMyrzakanov <Danntu1986#>
2020-05-02 16:55:43 +0600

    Initial commit

---
+++ Main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World")
+	fmt.Println("Hello World")
+	fmt.Printf("my int is %d",150)
+
+	a:="Hello"
+
+	fmt.Println("\na",a)
+
+}
